52b79c355df2827e49d86e9a5c2b4ce3c5919078
Matthew Monroe <matthew.monroe@pnnl.gov>
2006-08-08 18:10:08 +0000

    v1.0.0.3 - 2006 August 07 Now writing out peptide prophet values for every line in the input synopsis file

---
+++ PeptideProphetLibrary/DatasetNumMap.h
@@ -0,0 +1,19 @@
+#ifndef DATASETNUMMAP_H
+#define DATASETNUMMAP_H
+
+class DatasetNumMap
+{
+public:
+	DatasetNumMap() 
+	{
+		dataset_num_start = 0 ;
+		dataset_num_other = 0 ;
+	} ;
+
+	~DatasetNumMap() {};
+
+	int dataset_num_start ;
+	int dataset_num_other ;
+} ;
+
+#endif
---
+++ PeptideProphetLibrary/OutputContent.h
@@ -0,0 +1,23 @@
+#ifndef OUTPUTCONTENT_H
+#define OUTPUTCONTENT_H
+
+class OutputContent
+{
+public:
+	OutputContent()
+	{
+		HitNum = 0 ;
+		fscore = 0.0 ;
+		prob = 0.0 ;
+		negonly = 0 ;
+	} ;
+
+	~OutputContent() {};
+
+	int HitNum ;
+	float fscore ;
+	float prob ;
+	int negonly ;
+} ;
+
+#endif
